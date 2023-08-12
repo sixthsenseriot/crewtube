caecd931b8f375bf1dba096cc783df415ebe3e34
Khanh <knh.nguyen2002@gmail.com>
2023-08-12 07:15:34 -0500

    Changed start and build in package.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 
+import Navbar from "./components/Navbar";
+import { Feed } from "@mui/icons-material";
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./component/ChannelDetail";
+import SearchFeed from "./component/SearchFood";
+
 function App() {
   <BrowserRouter>
     <Box sx={{ backgroundColor: "#000" }}>
